45c331ff465dbcb303eb89a7b0dd89102d81c006
Phillip Olsen <Phillip@Olsenz.com>
2020-07-24 16:28:17 -0500

    Setting up out React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://y2qkh7zqr0.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_oZbLRPPFW",
+      APP_CLIENT_ID: "569ocjkokmvdu8q7obq0qp7eu7",
+      IDENTITY_POOL_ID: "us-east-1:f19392e5-9ee3-44cd-bded-fca90e71c075"
+    }
+  };
